98d32ed472b03a63a97ddbcae2b29daa47e004bb
Israel Arias <iariaspanez@gmail.com>
2025-11-25 22:08:52 -0300

    chore: update template

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # print the hostname to verify we are running inside the container/VM
-    return f"Hello from Python Agent! Running on: {socket.gethostname()}\n"
+    return f"Hello from Python Agent! Running on: {socket.gethostname()}\n Bye! \n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
